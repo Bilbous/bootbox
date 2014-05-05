d97e37177080c4084980e453543bb9e12e52db8c
Cenatiempo Yannick <ycenatiempo@gmail.com>
2014-05-05 22:38:38 +0200

    add setTemplates to create own templates

--- bootbox.js
+++ bootbox.js
@@ -755,6 +755,20 @@
     $.extend(defaults, values);
   };
 
+  exports.setTemplates = function() {
+      var values = {};
+
+      if (arguments.length === 2) {
+          // allow passing of single key/value...
+          values[arguments[0]] = arguments[1];
+      } else {
+          // ... and as an object too
+          values = arguments[0];
+      }
+
+      $.extend(templates, values);
+  }
+  
   exports.hideAll = function() {
     $(".bootbox").modal("hide");
   };
